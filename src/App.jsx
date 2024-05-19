f252041913ed28dd5ac6f668e5422c170a6a4e56
ArBajpai <arpanbajpai2003@gmail.com>
2024-05-19 23:11:46 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Body from "./Components/Body"
+import './App.css'
+import { useEffect } from "react";
+function App() {
+  return (
+  <>
+  
+  
+
+
+    <div className="container">
+      <Body/>
+
+    </div>
+  
+  </>
+ 
+  )
+}
+
+export default App;
